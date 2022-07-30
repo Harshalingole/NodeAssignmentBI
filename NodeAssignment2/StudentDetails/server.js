a337c52a73fab7854f2359481b8e0b0a5d270f01
Harshalingole <harshal.ingole21@gmail.com>
2022-07-30 22:17:05 +0530

    Node 2

---
+++ NodeAssignment2/StudentDetails/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route  = require('./router');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/students',route)
+
+
+app.listen(4000,()=>{
+    console.log('http://localhost:4000');
+})
+
